99a9c18552c2032e36f166df8b8de4fe2b78f75d
Pablo Arce <josepa_arce@hotmail.com>
2023-11-08 13:02:47 -0600

    init

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const config = {
+    name: 'sample-express-app',
+    port: 3000,
+    host: '0.0.0.0',
+};
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.status(200).send('hello world! we are using docker');
+});
+
+app.listen(config.port, config.host, (e)=> {
+    if(e) {
+        throw new Error('Internal Server Error');
+    }
+    console.log(`${config.name} running on ${config.host}:${config.port}`);
+});
